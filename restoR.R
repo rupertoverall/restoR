7f0c8fa8cff7891cd75424c87375b970458a4625
Rupert Overall <35666678+rupertoverall@users.noreply.github.com>
2025-03-02 15:34:01 +0100

    Better detection of previous libraries.

--- restoR.R
+++ restoR.R
@@ -1,24 +1,48 @@
 # https://github.com/rupertoverall/restoR/
 
-sep = "/" # Not sure how this is handled in Windows.
-library = .libPaths()
-libpath = strsplit(library, split = sep, perl = T)[[1]]
-version.index = which(libpath == "Versions") + 1
-this.version.number = as.numeric(strsplit(libpath[version.index], "-")[[1]][1]) # MacOS now has platform identifers.
-all.versions = sapply(strsplit(list.files(paste(libpath[1:(version.index - 1)], collapse = sep)), "-"), "[", 1)
-all.version.numbers = suppressWarnings( as.numeric(sapply(strsplit(all.versions, "-"), "[", 1)) )
-all.version.order = order(as.numeric(all.version.numbers), decreasing = T)
-all.versions = all.versions[all.version.order]
-all.version.numbers = all.version.numbers[all.version.order]
-last.version = all.versions[which(all.version.numbers < this.version.number)[1]]
-last.library = libpath
-all.libraries = list.dirs(paste(libpath[1:(version.index - 1)], collapse = sep), recursive = FALSE)
-all.libraries.versions = sapply(all.libraries, function(library) strsplit(library, split = sep, perl = T)[[1]][version.index] )
-last.version.string = all.libraries.versions[which(grepl(last.version, all.libraries.versions))]
-last.library[version.index] = last.version.string
-last.library = paste(last.library, collapse = sep)
+sep = "/"
 
-previous.packages = installed.packages(lib.loc = last.library)
+# Different platforms store the library info differently. Work through some possibilities:
+# Get system information.
+system.info = list(
+  os = Sys.info()["sysname"],
+  r.version = paste(R.version$major, R.version$minor, sep="."),
+  r.upgrade.version = paste(R.version$major, as.integer(R.version$minor), sep="."),
+  user.home = path.expand("~")
+)
+# Get library paths.
+system.libraries = character(0)
+user.libraries = character(0)
+for(path in .libPaths()){
+  if(grepl(system.info$user.home, path, fixed = TRUE)){
+    user.libraries = c(user.libraries, path)
+  }else{
+    system.libraries = c(system.libraries, path)
+  }
+}
+
+# Work through each previously-installed library and collect the missing packages.
+previous.packages = do.call("rbind", lapply(c(user.libraries, system.libraries), function(library){
+	path.components = strsplit(library, sep)[[1]]
+	version.index = grep(system.info$r.upgrade.version, path.components)
+	all.paths = list.files
+	all.versions = list.files(paste(path.components[1:(version.index - 1)], collapse = sep))
+	all.version.order = order(suppressWarnings( as.numeric(sapply(strsplit(all.versions, "-"), "[", 1)) ), decreasing = TRUE)
+	last.version = all.versions[all.version.order[2]]
+	if(!is.na(last.version)){
+		previous.library.components = path.components
+		previous.library.components[version.index] = last.version
+		previous.library = paste(previous.library.components, collapse = sep)
+		return(installed.packages(lib.loc = previous.library))
+	}else{
+		#stop("There are no previous R installations to be found.")
+		return(NULL)
+	}
+}))
+
+##
+
+# Now install all of the missing packages.
 required.packages = setdiff(previous.packages[, "Package"], installed.packages()[, "Package"])
 if(!require("BiocManager", quietly = TRUE)) install.packages("BiocManager", ask = FALSE)
 for(package in required.packages){
